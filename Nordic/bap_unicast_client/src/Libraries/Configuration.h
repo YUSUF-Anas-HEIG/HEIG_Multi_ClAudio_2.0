944ec66b557b54cdc1837e206455247a373411d7
RemyDG <remy.digiovanni@heig-vd.ch>
2026-08-13 10:00:54 +0200

    Add first libraries

---
+++ Nordic/bap_unicast_client/src/Libraries/Configuration.h
@@ -0,0 +1,13 @@
+/* Configuration
+ * 
+ */
+
+/* Libraries to include
+ ********************************************************/
+#include <stdint.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+/* Declaration
+ ********************************************************/
---
+++ Nordic/bap_unicast_client/src/Libraries/Visual.h
@@ -0,0 +1,14 @@
+
+/* Libraries to include
+ ********************************************************/
+#include <stdint.h>
+
+/* Types definitions
+ ********************************************************/
+typedef struct led 
+{
+    uint8_t red;
+    uint8_t green;
+    uint8_t blue;
+}LED;
+
